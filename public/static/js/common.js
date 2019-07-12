2a8e38e567f0a64d12bb1b9a292d18ad2d7980ee
Vanillaye <1749533190@qq.com>
2019-07-12 10:40:30 +0800

    注册 登录

--- public/static/js/common.js
+++ public/static/js/common.js
@@ -1,8 +1,3 @@
-//打开字滑入效果
-window.onload = function(){
-	$(".connect p").eq(0).animate({"left":"0%"}, 600);
-	$(".connect p").eq(1).animate({"left":"0%"}, 400);
-};
 //jquery.validate表单验证
 $(document).ready(function(){
 	//登录表单验证
@@ -10,27 +5,27 @@ $(document).ready(function(){
 		rules:{
 			username:{
 				required:true,//必填
-				minlength:3, //最少6个字符
-				maxlength:32,//最多20个字符
+				minlength:6, //最少6个字符
+				maxlength:12,//最多12个字符
 			},
 			password:{
 				required:true,
-				minlength:3, 
-				maxlength:32,
+				minlength:6, 
+				maxlength:12,
 			},
 		},
 		//错误信息提示
 		messages:{
 			username:{
 				required:"必须填写用户名",
-				minlength:"用户名至少为3个字符",
-				maxlength:"用户名至多为32个字符",
+				minlength:"用户名至少为6个字符",
+				maxlength:"用户名至多为12个字符",
 				remote: "用户名已存在",
 			},
 			password:{
 				required:"必须填写密码",
-				minlength:"密码至少为3个字符",
-				maxlength:"密码至多为32个字符",
+				minlength:"密码至少为6个字符",
+				maxlength:"密码至多为12个字符",
 			},
 		},
 
@@ -40,17 +35,19 @@ $(document).ready(function(){
 		rules:{
 			username:{
 				required:true,//必填
-				minlength:3, //最少6个字符
-				maxlength:32,//最多20个字符
+				minlength:6, //最少6个字符
+				maxlength:12,//最多20个字符
+				remote:{
+				},
 			},
 			password:{
 				required:true,
-				minlength:3, 
-				maxlength:32,
+				minlength:6, 
+				maxlength:12,
 			},
 			confirm_password:{
 				required:true,
-				minlength:3,
+				minlength:6,
 				equalTo:'.password'
 			},
 		},
@@ -58,18 +55,18 @@ $(document).ready(function(){
 		messages:{
 			username:{
 				required:"必须填写用户名",
-				minlength:"用户名至少为3个字符",
-				maxlength:"用户名至多为32个字符",
+				minlength:"用户名至少为6个字符",
+				maxlength:"用户名至多为12个字符",
 				remote: "用户名已存在",
 			},
 			password:{
 				required:"必须填写密码",
-				minlength:"密码至少为3个字符",
-				maxlength:"密码至多为32个字符",
+				minlength:"密码至少为6个字符",
+				maxlength:"密码至多为12个字符",
 			},
 			confirm_password:{
 				required: "请再次输入密码",
-				minlength: "确认密码不能少于3个字符",
+				minlength: "确认密码不能少于6个字符",
 				equalTo: "两次输入密码不一致",//与另一个元素相同
 			},
 		},
